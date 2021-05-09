8212e1d188ab09159a46d9d45f6f07157997f31d
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2021-05-09 23:31:54 +0500

    initial commit

---
+++ app/Http/Livewire/Users.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+use Livewire\WithPagination;
+
+class Users extends Component
+{   
+   
+    use WithPagination;
+
+    public $perPage = 10;
+    public $search = '';
+    public $orderBy = 'id';
+    public $orderAsc = true;
+    
+    public function render()
+    {
+        return view('livewire.users', [
+            'users' => User::search($this->search)
+                ->orderBy($this->orderBy, $this->orderAsc ? 'asc' : 'desc')
+                ->simplePaginate($this->perPage),
+        ]);
+    }
+
+    private function resetInputFields(){
+        $this->name = '';
+        $this->email = '';
+        $this->password = '';
+    }
+
+    public function store()
+    {
+        $validatedDate = $this->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        
+        ]);
+        
+        User::create($validatedDate);
+
+        session()->flash('message', 'Users Created Successfully.');
+
+        $this->resetInputFields();
+
+        $this->emit('userStore'); // Close model to using to jquery
+
+    }
+
+  
+
+    public function edit($id)
+    {
+        $this->updateMode = true;
+        $user = User::where('id',$id)->first();
+        $this->user_id = $id;
+        $this->name = $user->name;
+        $this->email = $user->email;
+        
+    }
+
+    public function cancel()
+    {
+        $this->updateMode = false;
+        $this->resetInputFields();
+
+
+    }
+
+    public function update()
+    {
+        $validatedDate = $this->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        if ($this->user_id) {
+            $user = User::find($this->user_id);
+            $user->update([
+                'name' => $this->name,
+                'email' => $this->email,
+            ]);
+            $this->updateMode = false;
+            session()->flash('message', 'Users Updated Successfully.');
+            $this->resetInputFields();
+            
+
+        }
+    }
+
+    public function delete($id)
+    {
+        if($id){
+            User::where('id',$id)->delete();
+            session()->flash('message', 'Users Deleted Successfully.');
+        }
+    }
+}
